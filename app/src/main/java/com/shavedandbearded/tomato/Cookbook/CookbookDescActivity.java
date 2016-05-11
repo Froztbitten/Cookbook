1f1c426bce9e4d13c30ed54d02477d246a57d30b
Devon Phillips <devonsphillips@hotmail.com>
2016-05-10 22:48:39 -0400

    Update

--- app/src/main/java/com/shavedandbearded/tomato/Cookbook/CookbookDescActivity.java
+++ app/src/main/java/com/shavedandbearded/tomato/Cookbook/CookbookDescActivity.java
@@ -1,5 +1,6 @@
 package com.shavedandbearded.tomato.Cookbook;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -26,6 +27,8 @@ import com.shavedandbearded.tomato.AppContentProvider;
 import com.shavedandbearded.tomato.DB;
 import com.shavedandbearded.tomato.R;
 import com.shavedandbearded.tomato.SettingsActivity;
+
+import org.w3c.dom.Text;
 
 public class CookbookDescActivity extends AppCompatActivity
 {
@@ -66,29 +69,78 @@ public class CookbookDescActivity extends AppCompatActivity
         {
             LinearLayout linearLayout = (LinearLayout) findViewById(R.id.ingredients_row);
 
-            LinearLayout rows = new LinearLayout(CookbookDescActivity.this);
-            rows.setOrientation(LinearLayout.HORIZONTAL);
-            rows.setLayoutParams(new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT));
-
             ViewSwitcher viewSwitcher = new ViewSwitcher(CookbookDescActivity.this);
             viewSwitcher.setLayoutParams(new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT));
             viewSwitcher.setId(i);
 
-            EditText quantity = new EditText(CookbookDescActivity.this);
-            quantity.setLayoutParams(new android.widget.LinearLayout.LayoutParams(
+            LinearLayout rows1 = new LinearLayout(CookbookDescActivity.this);
+            rows1.setOrientation(LinearLayout.HORIZONTAL);
+            rows1.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            TextView quantity1 = new TextView(CookbookDescActivity.this);
+            quantity1.setLayoutParams(new android.widget.LinearLayout.LayoutParams(
                     ViewGroup.LayoutParams.WRAP_CONTENT,
                     ViewGroup.LayoutParams.WRAP_CONTENT));
-            quantity.setId(i + 100);
-            quantity.setText(Float.toString(cursor.getFloat(2)));
-            viewSwitcher.addView(quantity);
-            rows.addView(viewSwitcher);
+            quantity1.setId(i + 100);
+            quantity1.setText(Float.toString(cursor.getFloat(2)));
+            rows1.addView(quantity1);
 
+            TextView label1 = new TextView(CookbookDescActivity.this);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(20, 0, 0, 0);
+            label1.setLayoutParams(layoutParams);
+            label1.setId(i + 200);
+            label1.setText(cursor.getString(3));
+            rows1.addView(label1);
+
+            TextView name1 = new TextView(CookbookDescActivity.this);
+            layoutParams = new LinearLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(20, 0, 0, 0);
+            name1.setLayoutParams(layoutParams);
+            name1.setId(i + 300);
+            name1.setText(cursor.getString(1));
+            rows1.addView(name1);
+
+            viewSwitcher.addView(rows1);
+
+            LinearLayout rows2 = new LinearLayout(CookbookDescActivity.this);
+            rows2.setOrientation(LinearLayout.HORIZONTAL);
+            rows2.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            EditText quantity2 = new EditText(CookbookDescActivity.this);
+            quantity2.setLayoutParams(new android.widget.LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+            quantity2.setId(i + 400);
+            quantity2.setText(Float.toString(cursor.getFloat(2)));
+            rows2.addView(quantity2);
+
+            EditText label2 = new EditText(CookbookDescActivity.this);
+            layoutParams = new LinearLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(20, 0, 0, 0);
+            label2.setLayoutParams(layoutParams);
+            label2.setId(i + 500);
+            label2.setText(cursor.getString(3));
+            rows2.addView(label2);
+
+            EditText name2 = new EditText(CookbookDescActivity.this);
+            layoutParams = new LinearLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(20, 0, 0, 0);
+            name2.setLayoutParams(layoutParams);
+            name2.setId(i + 600);
+            name2.setText(cursor.getString(1));
+            rows2.addView(name2);
+
+            viewSwitcher.addView(rows2);
+
+            linearLayout.addView(viewSwitcher);
             i++;
-            linearLayout.addView(rows);
         } while(cursor.moveToNext());
         cursor.close();
     }
@@ -147,7 +199,11 @@ public class CookbookDescActivity extends AppCompatActivity
         }
         else if (id == R.id.action_edit)
         {
-
+            for(int j = 0; j < i; j++)
+            {
+                ViewSwitcher viewSwitcher = (ViewSwitcher) (CookbookDescActivity.this).findViewById(j);
+                viewSwitcher.getNextView();
+            }
         }
         return super.onOptionsItemSelected(item);
     }
